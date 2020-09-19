9b90221fe252fdea86ff9c60c3d7025213d2d72d
srooter <1153475545@qq.com>
2020-09-19 17:22:33 +0800

    ✨feat: 数据脱敏

--- src/Mfilter.js
+++ src/Mfilter.js
@@ -36,6 +36,39 @@ const Mfilter = {
     return str;
   },
   /**
+   * @description 脱敏字符串。
+   * @author Sroot
+   * @version 0.0.1
+   * @param {String} str 必传，需要过滤的字符串。
+   * @param {NUmber} start  必传，开始脱敏字符串的下标。
+   * @param {NUmber} end  必传，结束脱敏字符串的下标。
+   * @param {String} maskChar  必传，脱敏符号。                          
+   *
+   * @example
+   *     let res = Mfilter.filterMaskString("19910088273", 2, 9, "*");
+   *     console.log(res)
+   */
+  filterMaskString(str, start, end, maskChar) {
+    if (str == null) {
+      return "";
+    }
+    if (start < 0) {
+      start = 0;
+    }
+    if (end > str.length) {
+      end = str.length;
+    }
+    let maskLength = str.length - start - end;
+    if (maskLength == 0) {
+      return str;
+    }
+    let maskText = ''
+    for (var i = 0; i < maskLength; i++) {
+      maskText += maskChar;
+    }
+    return str.substring(0, start) + sbMaskString + str.substring(start + maskLength, str.length + end);
+  },
+  /**
    * @description 数组去重。
    * @author Sroot
    * @version 0.0.1
